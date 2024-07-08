a55e1f6a0c86cee16e6884a54ef463f05e847f1d
Harsh-0986 <harsh2102.dev@gmail.com>
2024-07-08 18:47:22 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <Button>Hello</Button>
+      <div>Hello world!</div>
+    </main>
+  );
+}
+
+export default App;
